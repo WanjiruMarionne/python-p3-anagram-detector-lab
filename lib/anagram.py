0729e4d43367cb7dae76565a4f20839e6b14ae42
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-03-07 07:56:52 +0300

    Completed Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, words):
+        return [word for word in words if word.lower() != self.word and sorted(word.lower()) == sorted(self.word)]
+        
